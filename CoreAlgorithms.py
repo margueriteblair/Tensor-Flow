c4fc91c6c52f35c2f192745c7e7163dc7fc0dc61
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-10 23:31:05 -0500

    notes

---
+++ CoreAlgorithms.py
@@ -0,0 +1,7 @@
+#In this notebook we'll be walking through 4 fundamental machine learning algorithms.
+#Tensor Flow (TF) is an incredibly large library, and it's not always necessary to memorize syntax
+#The algorithms we'll look at include:
+#Linear regression, classification, clustering, hidden markov models
+
+#Linear regression is one of the most basic forms of machine learning
+
